fc7908eb20946a0d1a5a1c04a574691408876d28
Panagiotis Nanousis <11467288+Nanousis@users.noreply.github.com>
2022-10-23 09:50:45 +0300

    Added comments on the code

--- Mybutton.cs
+++ Mybutton.cs
@@ -5,19 +5,20 @@ using UnityEngine.EventSystems;
 
 public class Mybutton : MonoBehaviour
 {
-    public bool isPressed;
+    public bool isPressed;          //our bool that returns true if the button is being pressed
 
-    public float dampenPress = 0;
-    public float sensitivity = 2f;
+    public float dampenPress = 0;   //this returns 1 if the button is pressed after a short delay.
+    public float sensitivity = 2f;  //How fast the above float turns to 1
     // Start is called before the first frame update
     void Start()
     {
-        SetUpButton();
+        SetUpButton();              //we set up our button eventTriggers
     }
 
     // Update is called once per frame
     void Update()
     {
+                                    // here we change the value of the dampenPress based on time passed
         if (isPressed)
         {
             dampenPress += sensitivity * Time.deltaTime;
@@ -30,15 +31,20 @@ public class Mybutton : MonoBehaviour
     }
     void SetUpButton()
     {
+        //we create a new event trigger
         EventTrigger trigger = gameObject.AddComponent<EventTrigger>();
+        
+        //we set up a new entry for our event trigger for PointerDown
         var pointerDown = new EventTrigger.Entry();
         pointerDown.eventID = EventTriggerType.PointerDown;
         pointerDown.callback.AddListener((e) => onClickDown());
-
+        
+        //we create an entry for PointerUp as well
         var pointerUp = new EventTrigger.Entry();
         pointerUp.eventID = EventTriggerType.PointerUp;
         pointerUp.callback.AddListener((e) => onClickUp());
 
+        //we push the changes to the EventTrigger
         trigger.triggers.Add(pointerDown);
         trigger.triggers.Add(pointerUp);
 
